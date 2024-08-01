e7b1792218a3eecad47095d8309e990398f599c7
nazo6 <git@nazo6.dev>
2024-08-01 14:42:48 +0900

    Add makefile.toml

--- keyboards/keyball61-nrf52840/build.bat
+++
@@ -1,5 +0,0 @@
-cargo build --release
-cd target\thumbv7em-none-eabihf\release
-..\..\..\utils\arm-none-eabi-objcopy.exe -Oihex keyball61-nrf52840 keyball61-nrf52840.hex
-python3 ..\..\..\utils\uf2conv.py keyball61-nrf52840.hex -c -b 0x26000 -f 0xADA52840
-copy flash.uf2 E:\
