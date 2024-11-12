1a499f8ad5241aefa8206df02836967a30281825
nazo6 <git@nazo6.dev>
2024-11-12 15:18:31 +0900

    chore(rp,double-tap-reset): rewrite to suppress warning

--- lib/rktk-drivers-rp/src/double_tap.rs
+++ lib/rktk-drivers-rp/src/double_tap.rs
@@ -1,8 +1,4 @@
-use core::{
-    cell::UnsafeCell,
-    mem::MaybeUninit,
-    ptr::{read_volatile, write_volatile},
-};
+use core::{mem::MaybeUninit, ptr::write_volatile};
 
 use embassy_time::{Duration, Timer};
 use rktk::interface::double_tap::DoubleTapResetDriver;
@@ -10,25 +6,27 @@ use rktk::interface::double_tap::DoubleTapResetDriver;
 const BOOTLOADER_MAGIC: u32 = 0xDEADBEEF;
 
 #[link_section = ".uninit.FLAG"]
-static mut FLAG: UnsafeCell<MaybeUninit<u32>> = UnsafeCell::new(MaybeUninit::uninit());
+static mut FLAG: MaybeUninit<u32> = MaybeUninit::uninit();
 
 pub struct DoubleTapResetRp;
 
 impl DoubleTapResetDriver for DoubleTapResetRp {
     async fn execute(&self, timeout: Duration) {
-        // CREDIT: rumcake (https://github.com/Univa/rumcake/blob/2fa47dce9ab2b2406dd5465ccc9ce7b23e5ffdb0/rumcake/src/hw/mod.rs)
         unsafe {
-            if read_volatile(FLAG.get().cast::<u32>()) == BOOTLOADER_MAGIC {
-                write_volatile(FLAG.get().cast(), 0);
+            let flag = core::ptr::read(&raw const FLAG);
+            let flag = flag.assume_init();
 
+            if flag == BOOTLOADER_MAGIC {
+                // double reset is triggered. rebooting to bootloader.
+                write_volatile(&raw mut FLAG, MaybeUninit::new(0));
                 embassy_rp::rom_data::reset_to_usb_boot(0, 0);
             }
 
-            write_volatile(FLAG.get().cast(), BOOTLOADER_MAGIC);
-
+            // write flag and wait for double reset
+            write_volatile(&raw mut FLAG, MaybeUninit::new(BOOTLOADER_MAGIC));
             Timer::after(timeout).await;
-
-            write_volatile(FLAG.get().cast(), 0);
+            // double-tap reset is not performed. reset flag and normal start
+            write_volatile(&raw mut FLAG, MaybeUninit::new(0));
         }
     }
 }
