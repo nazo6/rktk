bf379ad794e477cd866aa48d9ce590f0f098349a
nazo6 <git@nazo6.dev>
2024-11-13 16:46:34 +0900

    update

--- lib/rktk-rrp/src/lib.rs
+++ lib/rktk-rrp/src/lib.rs
@@ -26,10 +26,18 @@ pub mod next {
         pub(crate) use gen_type;
     }
 
+    #[derive(Debug, PartialEq, Eq)]
+    enum Indicator {
+        StreamContinues,
+        End,
+        Invalid,
+    }
+
     pub mod server {
-        use super::macro_utils::gen_type;
+        use super::{macro_utils::gen_type, Indicator};
         use embedded_io_async::{Read, Write};
         use futures::Stream;
+        use serde::de::DeserializeOwned;
 
         pub struct Server<T: Read + Write, H: ServerHandlers, const BUF_SIZE: usize> {
             transport: T,
@@ -78,7 +86,7 @@ pub mod next {
                 Ok(())
             }
 
-            /// Recives body size. Must be called exactly after other header is recieved.
+            /// Recives body size. Must be called exactly after other headers are recieved.
             async fn recv_body_size(
                 &mut self,
             ) -> Result<u32, embedded_io_async::ReadExactError<T::Error>> {
@@ -87,26 +95,66 @@ pub mod next {
                 Ok(u32::from_le_bytes(buf))
             }
 
-            async fn recv_request_body<'a>(
+            async fn recv_indicator(
                 &mut self,
-                buf: &'a mut [u8],
-            ) -> Result<&'a [u8], embedded_io_async::ReadExactError<T::Error>> {
-                let request_size = self.recv_body_size().await?;
+            ) -> Result<super::Indicator, embedded_io_async::ReadExactError<T::Error>> {
+                let mut buf = [0u8; 1];
+                self.transport.read_exact(&mut buf).await?;
+                let indicator = match buf[0] {
+                    0x00 => super::Indicator::End,
+                    0xFF => super::Indicator::StreamContinues,
+                    _ => super::Indicator::Invalid,
+                };
+                Ok(indicator)
+            }
+
+            async fn recv_request_body<R: DeserializeOwned>(
+                &mut self,
+                buf: &mut [u8],
+            ) -> Result<(R, Indicator), &'static str> {
+                let request_size = self
+                    .recv_body_size()
+                    .await
+                    .map_err(|_| "Failed to receive size")?;
 
                 self.transport
                     .read_exact(&mut buf[0..request_size as usize])
-                    .await?;
-                Ok(&buf[0..request_size as usize])
+                    .await
+                    .map_err(|_| "Failed to receive data")?;
+
+                let deserialized = postcard::from_bytes::<R>(&buf[0..request_size as usize])
+                    .map_err(|_| "Failed to deserialize")?;
+
+                let indicator = self
+                    .recv_indicator()
+                    .await
+                    .map_err(|_| "Failed to receive indicator")?;
+
+                Ok((deserialized, indicator))
             }
 
-            fn recv_stream_request<'a>(
-                &'a mut self,
+            fn recv_stream_request<'a, 's: 'a, R: DeserializeOwned>(
+                &'s mut self,
                 buf: &'a mut [u8],
-            ) -> impl Stream<Item = Result<&'a [u8], ReadExactError<T>>> + 'a {
-                futures::stream::unfold((self, buf), move |s| async move {
-                    let res = s.0.recv_request_body(s.1).await;
-                    Some((res, (s.0, s.1)))
-                })
+            ) -> impl Stream<Item = Result<R, &'static str>> + 'a {
+                futures::stream::unfold(
+                    (self, buf, false),
+                    move |(s, buf, mut stream_finished)| async move {
+                        if stream_finished {
+                            return None;
+                        }
+
+                        let Ok((res, indicator)) = s.recv_request_body(buf).await else {
+                            return None;
+                        };
+
+                        if indicator == Indicator::End {
+                            stream_finished = true;
+                        }
+
+                        Some((Ok(res), (s, buf, stream_finished)))
+                    },
+                )
             }
         }
 
@@ -126,7 +174,8 @@ pub mod next {
                         match endpoint_id {
                             $(
                                 $endpoint_id => {
-                                    Ok(())
+                                    let buf = [0u8; 256];
+                                    let res = generate_server_handlers!(@req_handler $req_kind, self, $endpoint_name, &mut buf);
                                 }
                             )*
                             _ => {
@@ -135,8 +184,12 @@ pub mod next {
                         }
                     }
                 }
-
             };
+
+            (@req_handler normal, $self:expr, $endpoint_name:ident, $buf:expr) => {{
+                let req = $self.recv_request_body($buf).await.map(|(req, _)| req)?;
+                $self.handlers.$endpoint_name(req).await
+            }}
         }
 
         generate_server_handlers!(
