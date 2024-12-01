03ecf40f5d2478ce4e94a20714d641c3f70f93bf
nazo6 <git@nazo6.dev>
2024-12-02 02:59:39 +0900

    chore: fix build in docs.rs

--- lib/rktk/build/main.rs
+++ lib/rktk/build/main.rs
@@ -1,22 +1,36 @@
 use schemars::schema_for;
 
+mod rktk_json_docsrs;
 mod ser_codegen;
 
 include!("../src/config/static_config/schema.rs");
 
 fn main() {
-    let config_path = std::env::var("RKTK_CONFIG_PATH").expect("RKTK_CONFIG_PATH is not set");
+    let config_path = if std::env::var("DOCS_RS").is_ok() {
+        // in docs.rs rktk.json cannot be loaded.
+        String::new()
+    } else {
+        std::env::var("RKTK_CONFIG_PATH").expect("RKTK_CONFIG_PATH is not set")
+    };
 
     // println!("cargo:warning=Using config: {}", config_path);
     // println!("cargo:warning=current_time:{:?}", std::time::Instant::now());
 
     println!("cargo:rerun-if-env-changed=RKTK_CONFIG_PATH");
+    println!("cargo:rerun-if-env-changed=DOCS_RS");
     println!("cargo:rerun-if-changed={}", config_path);
     println!("cargo:rerun-if-changed=src/config/static_config/schema.rs");
 
     println!("cargo:rustc-cfg=no_build");
 
-    let config = std::fs::read_to_string(config_path).expect("Failed to read config file");
+    let config = if std::env::var("DOCS_RS").is_ok() {
+        // in docs.rs use demo config insted.
+        println!("cargo:warning=Using demo json for docs.rs");
+        rktk_json_docsrs::RKTK_JSON_DOCSRS.to_string()
+    } else {
+        std::fs::read_to_string(config_path).expect("Failed to read config file")
+    };
+
     let config: StaticConfig = serde_json::from_str(&config).expect("Failed to parse config file");
 
     let code = format!(
