155d2af90059d7361feacfdb7a9045345aff0829
nazo6 <git@nazo6.dev>
2024-12-01 13:11:55 +0900

    chore: cleanup

--- lib/rktk/src/task/main_loop/master.rs
+++ lib/rktk/src/task/main_loop/master.rs
@@ -340,29 +340,26 @@ pub async fn start<
                     },
                     // mouse
                     async {
-                        let mut empty_sent = false;
-                        loop {
-                            Timer::after(SCAN_INTERVAL_MOUSE).await;
+                        if let Some(mouse) = &mut mouse {
+                            let mut empty_sent = false;
+                            loop {
+                                Timer::after(SCAN_INTERVAL_MOUSE).await;
 
-                            let mut mouse_move = (0, 0);
-                            if let Some(mouse) = &mut mouse {
-                                match mouse.read().await {
-                                    Ok((x, y)) => {
-                                        mouse_move.0 += x;
-                                        mouse_move.1 += y;
-                                    }
+                                let mouse_move = match mouse.read().await {
+                                    Ok(m) => m,
                                     Err(e) => {
                                         log::warn!("Failed to read mouse: {:?}", e);
                                         crate::print!("{:?}", e);
+                                        continue;
                                     }
-                                }
-                            }
+                                };
 
-                            if mouse_move == (0, 0) && empty_sent {
-                                continue;
-                            } else {
-                                let _ = mouse_move_ch_sender.try_send(mouse_move);
-                                empty_sent = mouse_move == (0, 0);
+                                if mouse_move == (0, 0) && empty_sent {
+                                    continue;
+                                } else {
+                                    let _ = mouse_move_ch_sender.try_send(mouse_move);
+                                    empty_sent = mouse_move == (0, 0);
+                                }
                             }
                         }
                     },
