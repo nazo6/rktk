0435b4fd6fd7bd83ca996b4cecf168d16eca9138
nazo6 <git@nazo6.dev>
2024-12-17 12:29:26 +0900

    feat(drivers/nrf): Add full-duplex uart split driver

--- lib/rktk-drivers-nrf/src/split/mod.rs
+++ lib/rktk-drivers-nrf/src/split/mod.rs
@@ -1,2 +1,2 @@
+pub mod uart_full_duplex;
 pub mod uart_half_duplex;
-// pub mod uart_half_duplex_rx_only;
---
+++ lib/rktk-drivers-nrf/src/split/uart_full_duplex.rs
@@ -0,0 +1,52 @@
+use embassy_nrf::buffered_uarte::BufferedUarte;
+use embassy_nrf::timer::Instance as TimerInstance;
+use embassy_nrf::uarte::Instance as UarteInstance;
+use embedded_io_async::{Read, Write as _};
+use rktk::drivers::interface::split::SplitDriver;
+
+#[derive(Debug, thiserror::Error)]
+pub enum UartFullDuplexSplitDriverError {
+    #[error("General error: {0}")]
+    GeneralError(&'static str),
+}
+
+pub struct UartFullDuplexSplitDriver<'d, I: UarteInstance, T: TimerInstance> {
+    uarte: BufferedUarte<'d, I, T>,
+}
+
+impl<'d, I: UarteInstance, T: TimerInstance> UartFullDuplexSplitDriver<'d, I, T> {
+    pub fn new(uarte: BufferedUarte<'d, I, T>) -> Self {
+        Self { uarte }
+    }
+}
+
+impl<I: UarteInstance, T: TimerInstance> SplitDriver for UartFullDuplexSplitDriver<'_, I, T> {
+    type Error = UartFullDuplexSplitDriverError;
+
+    async fn wait_recv(&mut self, buf: &mut [u8], is_master: bool) -> Result<(), Self::Error> {
+        let mut i = 0;
+        loop {
+            let mut byte = [0];
+            self.uarte
+                .read_exact(&mut byte)
+                .await
+                .map_err(|_| UartFullDuplexSplitDriverError::GeneralError("Read error"))?;
+            if byte[0] == 0 {
+                break;
+            } else {
+                buf[i] = byte[0];
+                i += 1;
+            }
+        }
+        log::info!("Received: {:?}", buf);
+        Ok(())
+    }
+
+    async fn send(&mut self, buf: &[u8], is_master: bool) -> Result<(), Self::Error> {
+        rktk::print!("Sending: {:?}\n", buf);
+        self.uarte
+            .write_all(buf)
+            .await
+            .map_err(|_| UartFullDuplexSplitDriverError::GeneralError("Write error"))
+    }
+}
