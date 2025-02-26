aa29f27bb23db20b711acdf7ec82ff8c491410bf
nazo6 <git@nazo6.dev>
2025-02-27 01:35:38 +0900

    reporter impl

--- lib/rktk-drivers-nrf/src/dongle/reporter.rs
+++ lib/rktk-drivers-nrf/src/dongle/reporter.rs
@@ -3,9 +3,36 @@ use core::convert::Infallible;
 use embassy_nrf::radio::Instance;
 use embassy_nrf_esb::ptx::PtxRadio;
 use rktk::drivers::interface::{
+    ble::BleDriver,
     dongle::{DongleData, KeyboardReport},
     reporter::ReporterDriver,
+    BackgroundTask, DriverBuilderWithTask,
 };
+
+pub struct EsbReporterDriverBuilder<T: Instance>(pub EsbReporterDriver<T>);
+
+struct DummyTask;
+impl BackgroundTask for DummyTask {
+    async fn run(self) {}
+}
+
+impl<T: Instance> DriverBuilderWithTask for EsbReporterDriverBuilder<T> {
+    type Driver = EsbReporterDriver<T>;
+
+    type Error = ();
+
+    async fn build(
+        self,
+    ) -> Result<
+        (
+            Self::Driver,
+            impl rktk::drivers::interface::BackgroundTask + 'static,
+        ),
+        Self::Error,
+    > {
+        Ok((self.0, DummyTask))
+    }
+}
 
 pub struct EsbReporterDriver<T: Instance> {
     ptx: PtxRadio<'static, T, 64>,
@@ -57,7 +84,7 @@ impl<T: Instance> ReporterDriver for EsbReporterDriver<T> {
         let report = DongleData::Keyboard(report);
         let mut buf = [0u8; 64];
         if let Ok(data) = postcard::to_slice(&report, &mut buf) {
-            self.ptx.send(0, data, false);
+            self.ptx.send(0, data, false).await;
         }
 
         Ok(())
@@ -70,9 +97,17 @@ impl<T: Instance> ReporterDriver for EsbReporterDriver<T> {
         let report = DongleData::Mouse(report.into());
         let mut buf = [0u8; 64];
         if let Ok(data) = postcard::to_slice(&report, &mut buf) {
-            self.ptx.send(0, data, false);
+            self.ptx.send(0, data, false).await;
         }
 
         Ok(())
+    }
+}
+
+impl<T: Instance> BleDriver for EsbReporterDriver<T> {
+    type Error = Infallible;
+
+    async fn clear_bond_data(&self) -> Result<(), <Self as BleDriver>::Error> {
+        todo!()
     }
 }
