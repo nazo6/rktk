ac84bfb43951958e54ed3cc0c7bc761f3dfcf8e9
nazo6 <git@nazo6.dev>
2025-03-04 14:32:06 +0900

    a

--- lib/rktk-drivers-nrf/src/dongle/dongle.rs
+++ lib/rktk-drivers-nrf/src/dongle/dongle.rs
@@ -1,57 +1,138 @@
-use embassy_nrf::radio::Instance;
-use embassy_nrf_esb::prx::{PrxInterface, PrxTask};
+use core::{marker::PhantomData, sync::atomic::AtomicBool};
+
+use embassy_nrf::{
+    interrupt,
+    radio::{self},
+    timer,
+};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
+use esb_ng::{
+    bbq2::queue::BBQueue, irq::StatePRX, peripherals::PtrTimer as _, Addresses, ConfigBuilder,
+    EsbApp, EsbBuffer, EsbIrq, IrqTimer,
+};
 use rktk::drivers::interface::{
     dongle::{DongleData, DongleDriver},
     BackgroundTask, DriverBuilderWithTask,
 };
 
-// ---- Builder -------
-
-pub struct EsbDongleDriverBuilder<T: Instance> {
-    pub prx_task: PrxTask<T, 255>,
-    pub prx_interface: PrxInterface,
+macro_rules! use_peripheral {
+    ($radio:ident, $timer:ident, $esb_timer:ident) => {
+        type DongleRadio = embassy_nrf::peripherals::$radio;
+        const DONGLE_RADIO_PAC: embassy_nrf::pac::radio::Radio = embassy_nrf::pac::$radio;
+        type DongleTimer = embassy_nrf::peripherals::$timer;
+        type DongleTimerEsb = esb_ng::peripherals::$esb_timer;
+    };
 }
 
-impl<T: Instance> DriverBuilderWithTask for EsbDongleDriverBuilder<T> {
+use_peripheral!(RADIO, TIMER0, Timer0);
+
+static IRQ_TIMER: Mutex<CriticalSectionRawMutex, Option<IrqTimer<DongleTimerEsb>>> =
+    Mutex::new(None);
+
+pub struct TimerInterruptHandler {
+    _phantom: PhantomData<()>,
+}
+
+impl interrupt::typelevel::Handler<<DongleTimer as timer::Instance>::Interrupt>
+    for TimerInterruptHandler
+{
+    unsafe fn on_interrupt() {
+        if let Ok(mut irq_timer) = IRQ_TIMER.try_lock() {
+            if let Some(irq_timer) = &mut *irq_timer {
+                rktk_log::debug!("Timer interrupt");
+                irq_timer.timer_interrupt();
+            }
+        }
+    }
+}
+
+static ESB_IRQ: Mutex<
+    CriticalSectionRawMutex,
+    Option<EsbIrq<1024, 1024, DongleTimerEsb, StatePRX>>,
+> = Mutex::new(None);
+
+pub struct EsbInterruptHandler {
+    _phantom: PhantomData<()>,
+}
+
+impl interrupt::typelevel::Handler<<DongleRadio as radio::Instance>::Interrupt>
+    for EsbInterruptHandler
+{
+    unsafe fn on_interrupt() {
+        if let Ok(mut esb_irq) = ESB_IRQ.try_lock() {
+            if let Some(esb_irq) = &mut *esb_irq {
+                rktk_log::debug!("Radio interrupt");
+                esb_irq.radio_interrupt();
+            }
+        }
+    }
+}
+
+// ---- Builder -------
+
+pub struct EsbDongleDriverBuilder {
+    pub timer: DongleTimer,
+    pub radio: DongleRadio,
+}
+
+impl DriverBuilderWithTask for EsbDongleDriverBuilder {
     type Driver = EsbDongleDriver;
 
     type Error = ();
 
     async fn build(self) -> Result<(Self::Driver, impl BackgroundTask + 'static), Self::Error> {
+        static BUFFER: EsbBuffer<1024, 1024> = EsbBuffer {
+            app_to_radio_buf: BBQueue::new(),
+            radio_to_app_buf: BBQueue::new(),
+            timer_flag: AtomicBool::new(false),
+        };
+        let addresses = Addresses::default();
+        let config = ConfigBuilder::default()
+            .maximum_transmit_attempts(0)
+            .max_payload_size(192)
+            .check()
+            .unwrap();
+        let (esb_app, esb_irq, esb_timer) = BUFFER
+            .try_split(
+                unsafe { DongleTimerEsb::take() },
+                DONGLE_RADIO_PAC,
+                addresses,
+                config,
+            )
+            .unwrap();
+        let mut esb_irq = esb_irq.into_prx();
+        esb_irq.start_receiving().unwrap();
+        ESB_IRQ.lock().await.replace(esb_irq);
+        IRQ_TIMER.lock().await.replace(esb_timer);
+
         Ok((
             EsbDongleDriver {
-                prx_interface: self.prx_interface,
+                esb: esb_app,
                 cnt: 0,
             },
-            EsbDongleDriverTask {
-                prx_task: self.prx_task,
-            },
+            EsbDongleDriverTask {},
         ))
     }
 }
 
 // ---- Task ------
 
-pub struct EsbDongleDriverTask<T: Instance> {
-    prx_task: PrxTask<T, 255>,
-}
+pub struct EsbDongleDriverTask {}
 
-impl<T: Instance> BackgroundTask for EsbDongleDriverTask<T> {
-    async fn run(mut self) {
-        self.prx_task.run().await;
-    }
+impl BackgroundTask for EsbDongleDriverTask {
+    async fn run(self) {}
 }
 
 // ----- Driver -------
 
 pub struct EsbDongleDriver {
-    prx_interface: PrxInterface,
+    esb: EsbApp<1024, 1024>,
     cnt: u8,
 }
 
 #[derive(Debug)]
 pub enum EsbDongleError {
-    Esb(embassy_nrf_esb::Error),
+    Esb(esb_ng::Error),
     Deserialization(postcard::Error),
 }
 
@@ -59,14 +140,10 @@ impl DongleDriver for EsbDongleDriver {
     type Error = EsbDongleError;
 
     async fn recv(&mut self) -> Result<DongleData, Self::Error> {
-        let mut buf = [0; 64];
-        let size = self
-            .prx_interface
-            .recv(&mut buf)
-            .await
-            .map_err(EsbDongleError::Esb)?;
+        let payload = self.esb.wait_read_packet().await;
+        rktk_log::debug!("packet received");
         let (cnt, data): (u8, DongleData) =
-            postcard::from_bytes(&buf[..size]).map_err(EsbDongleError::Deserialization)?;
+            postcard::from_bytes(&payload).map_err(EsbDongleError::Deserialization)?;
 
         if cnt.wrapping_sub(self.cnt) > 1 {
             rktk_log::warn!("Packet dropped: {} -> {}", self.cnt, cnt);
--- lib/rktk-drivers-nrf/src/dongle/reporter.rs
+++ lib/rktk-drivers-nrf/src/dongle/reporter.rs
@@ -1,75 +1,182 @@
-use core::convert::Infallible;
+use core::{convert::Infallible, marker::PhantomData, sync::atomic::AtomicBool};
 
-use embassy_nrf::{interrupt, radio::Instance, Peripheral};
-pub use embassy_nrf_esb::ptx;
-use embassy_nrf_esb::{
-    ptx::{new_ptx, PtxConfig, PtxInterface, PtxTask},
-    InterruptHandler, RadioConfig,
+use embassy_nrf::{
+    interrupt::{self, typelevel::Binding},
+    radio::{self},
+    timer,
+};
+use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
+use esb_ng::{
+    bbq2::queue::BBQueue, irq::StatePTX, peripherals::PtrTimer as _, Addresses, ConfigBuilder,
+    EsbApp, EsbBuffer, EsbHeader, EsbIrq, IrqTimer,
 };
 use postcard::experimental::max_size::MaxSize as _;
-use rktk::drivers::interface::{
-    ble::BleDriver, dongle::DongleData, reporter::ReporterDriver, BackgroundTask,
-    DriverBuilderWithTask,
+use rktk::{
+    drivers::interface::{
+        ble::BleDriver, dongle::DongleData, reporter::ReporterDriver, BackgroundTask,
+        DriverBuilderWithTask,
+    },
+    utils::Channel,
 };
+use rktk_log::{debug, warn};
 
-// -------- Builder ----------
-
-pub struct EsbReporterDriverBuilder<T: Instance> {
-    pub ptx_task: PtxTask<T, 255>,
-    pub ptx_interface: PtxInterface,
+macro_rules! use_peripheral {
+    ($radio:ident, $timer:ident, $esb_timer:ident) => {
+        type DongleRadio = embassy_nrf::peripherals::$radio;
+        const DONGLE_RADIO_PAC: embassy_nrf::pac::radio::Radio = embassy_nrf::pac::$radio;
+        type DongleTimer = embassy_nrf::peripherals::$timer;
+        type DongleTimerEsb = esb_ng::peripherals::$esb_timer;
+    };
 }
 
-impl<T: Instance> EsbReporterDriverBuilder<T> {
-    pub fn new(
-        radio: impl Peripheral<P = T> + 'static,
-        irq: impl interrupt::typelevel::Binding<T::Interrupt, InterruptHandler<T>> + 'static,
-        config: RadioConfig,
-        ptx_config: PtxConfig,
-    ) -> Self {
-        let (ptx_task, ptx_interface) = new_ptx(radio, irq, config, ptx_config);
+use_peripheral!(RADIO, TIMER0, Timer0);
 
-        Self {
-            ptx_task,
-            ptx_interface,
+static IRQ_TIMER: Mutex<CriticalSectionRawMutex, Option<IrqTimer<DongleTimerEsb>>> =
+    Mutex::new(None);
+
+pub struct TimerInterruptHandler {
+    _phantom: PhantomData<()>,
+}
+
+impl interrupt::typelevel::Handler<<DongleTimer as timer::Instance>::Interrupt>
+    for TimerInterruptHandler
+{
+    unsafe fn on_interrupt() {
+        panic!("reach interrupt");
+        if let Ok(mut irq_timer) = IRQ_TIMER.try_lock() {
+            if let Some(irq_timer) = &mut *irq_timer {
+                irq_timer.timer_interrupt();
+            }
         }
     }
 }
 
-impl<T: Instance> DriverBuilderWithTask for EsbReporterDriverBuilder<T> {
+static ESB_IRQ: Mutex<
+    CriticalSectionRawMutex,
+    Option<EsbIrq<1024, 1024, DongleTimerEsb, StatePTX>>,
+> = Mutex::new(None);
+
+pub struct EsbInterruptHandler {
+    _phantom: PhantomData<()>,
+}
+
+impl interrupt::typelevel::Handler<<DongleRadio as radio::Instance>::Interrupt>
+    for EsbInterruptHandler
+{
+    unsafe fn on_interrupt() {
+        panic!("reach interrupt");
+        if let Ok(mut esb_irq) = ESB_IRQ.try_lock() {
+            if let Some(esb_irq) = &mut *esb_irq {
+                esb_irq.radio_interrupt();
+            }
+        }
+    }
+}
+
+// -------- Builder ----------
+
+static REPORT_SEND_CHAN: Channel<DongleData, 16> = Channel::new();
+const MAX_PAYLOAD_SIZE: u8 = 192;
+
+pub struct EsbReporterDriverBuilder {
+    _phantom: PhantomData<()>,
+}
+
+impl EsbReporterDriverBuilder {
+    pub fn new(
+        _timer: DongleTimer,
+        _radio: DongleRadio,
+        _irqs: impl Binding<<DongleTimer as timer::Instance>::Interrupt, TimerInterruptHandler>
+            + Binding<<DongleRadio as radio::Instance>::Interrupt, EsbInterruptHandler>,
+    ) -> Self {
+        Self {
+            _phantom: PhantomData,
+        }
+    }
+}
+
+impl DriverBuilderWithTask for EsbReporterDriverBuilder {
     type Driver = EsbReporterDriver;
 
     type Error = ();
 
     async fn build(self) -> Result<(Self::Driver, impl BackgroundTask + 'static), Self::Error> {
-        Ok((
-            EsbReporterDriver {
-                ptx_interface: self.ptx_interface,
-                cnt: core::sync::atomic::AtomicU8::new(0),
-            },
-            Task {
-                ptx_task: self.ptx_task,
-            },
-        ))
+        static BUFFER: EsbBuffer<1024, 1024> = EsbBuffer {
+            app_to_radio_buf: BBQueue::new(),
+            radio_to_app_buf: BBQueue::new(),
+            timer_flag: AtomicBool::new(false),
+        };
+        let addresses = Addresses::default();
+        let config = ConfigBuilder::default()
+            .maximum_transmit_attempts(0)
+            .max_payload_size(MAX_PAYLOAD_SIZE)
+            .check()
+            .unwrap();
+
+        let (esb_app, esb_irq, esb_timer) = BUFFER
+            .try_split(
+                unsafe { DongleTimerEsb::take() },
+                DONGLE_RADIO_PAC,
+                addresses,
+                config,
+            )
+            .unwrap();
+        let esb_irq = esb_irq.into_ptx();
+        ESB_IRQ.lock().await.replace(esb_irq);
+        IRQ_TIMER.lock().await.replace(esb_timer);
+
+        Ok((EsbReporterDriver {}, Task { esb_app }))
     }
 }
 
 // --------- Task ----------
 
-struct Task<T: Instance> {
-    ptx_task: PtxTask<T, 255>,
+struct Task {
+    esb_app: EsbApp<1024, 1024>,
 }
-impl<T: Instance> BackgroundTask for Task<T> {
+impl BackgroundTask for Task {
     async fn run(mut self) {
-        self.ptx_task.run().await;
+        let mut cnt = 0;
+        let mut pid = 0;
+        loop {
+            let report = REPORT_SEND_CHAN.receive().await;
+            debug!("Received report: {:?}", report);
+            let mut buf = [0; DongleDataWithCnt::POSTCARD_MAX_SIZE];
+            let Ok(slice) = postcard::to_slice(&(cnt, report), &mut buf) else {
+                warn!("Postcard error");
+                continue;
+            };
+
+            let esb_header = EsbHeader::build()
+                .max_payload(MAX_PAYLOAD_SIZE)
+                .pid(pid)
+                .pipe(0)
+                .no_ack(false)
+                .check()
+                .unwrap();
+            let Ok(mut packet) = self.esb_app.grant_packet(esb_header) else {
+                warn!("Packet full");
+                continue;
+            };
+            packet[..slice.len()].copy_from_slice(slice);
+            packet.commit(slice.len());
+            self.esb_app.start_tx();
+
+            debug!("Sent report: {:?}", slice);
+
+            cnt += 1;
+            if pid == 3 {
+                pid = 0;
+            } else {
+                pid += 1;
+            }
+        }
     }
 }
 
 // ----------- Driver ------------
 
-pub struct EsbReporterDriver {
-    ptx_interface: PtxInterface,
-    cnt: core::sync::atomic::AtomicU8,
-}
+pub struct EsbReporterDriver {}
 
 #[derive(Debug)]
 pub struct ErrorMsg(&'static str);
@@ -82,18 +189,6 @@ impl core::error::Error for ErrorMsg {}
 
 type DongleDataWithCnt = (usize, DongleData);
 
-impl EsbReporterDriver {
-    fn send_report(&self, report: DongleData) -> Result<(), ErrorMsg> {
-        let mut buf = [0; DongleDataWithCnt::POSTCARD_MAX_SIZE];
-        let cnt = self.cnt.fetch_add(1, core::sync::atomic::Ordering::Relaxed);
-        postcard::to_slice(&(cnt, report), &mut buf).map_err(|_| ErrorMsg("ser"))?;
-        self.ptx_interface
-            .try_send(0, &buf, false)
-            .map_err(|_| ErrorMsg("ch full"))?;
-        Ok(())
-    }
-}
-
 impl ReporterDriver for EsbReporterDriver {
     type Error = ErrorMsg;
 
@@ -101,21 +196,27 @@ impl ReporterDriver for EsbReporterDriver {
         &self,
         report: usbd_hid::descriptor::KeyboardReport,
     ) -> Result<(), Self::Error> {
-        self.send_report(DongleData::Keyboard(report.into()))
+        REPORT_SEND_CHAN
+            .try_send(DongleData::Keyboard(report.into()))
+            .map_err(|_| ErrorMsg("Send error"))
     }
 
     fn try_send_media_keyboard_report(
         &self,
         report: usbd_hid::descriptor::MediaKeyboardReport,
     ) -> Result<(), Self::Error> {
-        self.send_report(DongleData::MediaKeyboard(report.into()))
+        REPORT_SEND_CHAN
+            .try_send(DongleData::MediaKeyboard(report.into()))
+            .map_err(|_| ErrorMsg("Send error"))
     }
 
     fn try_send_mouse_report(
         &self,
         report: usbd_hid::descriptor::MouseReport,
     ) -> Result<(), Self::Error> {
-        self.send_report(DongleData::Mouse(report.into()))
+        REPORT_SEND_CHAN
+            .try_send(DongleData::Mouse(report.into()))
+            .map_err(|_| ErrorMsg("Send error"))
     }
 
     async fn send_rrp_data(&self, _data: &[u8]) -> Result<(), Self::Error> {
