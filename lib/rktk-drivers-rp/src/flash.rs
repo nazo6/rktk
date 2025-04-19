f40124703e8c74e4be893117b2fbc110800a6ac1
nazo6 <git@nazo6.dev>
2025-04-19 13:44:37 +0900

    chore(rp): Change init_storage macro to not need to define variable

--- lib/rktk-drivers-rp/src/flash.rs
+++ lib/rktk-drivers-rp/src/flash.rs
@@ -1,10 +1,10 @@
 pub use embassy_sync;
-pub use rktk_drivers_common::storage::flash_sequential_map::sequential_storage;
 pub use rktk_drivers_common::storage::flash_sequential_map::FlashSequentialMapStorage;
+pub use rktk_drivers_common::storage::flash_sequential_map::sequential_storage;
 
 #[macro_export]
 macro_rules! init_storage {
-    ($storage:expr, $flash:expr, $dma:expr, $size:expr) => {
+    ($storage:ident, $flash:expr, $dma:expr, $size:expr) => {
         let flash = ::embassy_rp::flash::Flash::<_, _, $size>::new($flash, $dma);
         let flash = $crate::flash::embassy_sync::mutex::Mutex::new(flash);
         let cache = $crate::flash::embassy_sync::mutex::Mutex::new(
@@ -14,10 +14,10 @@ macro_rules! init_storage {
         const FLASH_START: u32 = 1024 * 1024;
         const FLASH_END: u32 = 3 * 1024 * 1024;
 
-        $storage = $crate::flash::FlashSequentialMapStorage {
+        let $storage = $crate::flash::FlashSequentialMapStorage {
             flash: &flash,
             flash_range: FLASH_START..FLASH_END,
             cache: &cache,
-        }
+        };
     };
 }
