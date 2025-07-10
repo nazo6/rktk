9c348a836519ca854e33c9a307467fa8e1c60fe6
nazo6 <git@nazo6.dev>
2025-07-10 16:52:15 +0900

    chore: Fix warnings

--- lib/rktk-drivers-common/src/usb/handler.rs
+++ lib/rktk-drivers-common/src/usb/handler.rs
@@ -1,9 +1,5 @@
 use core::sync::atomic::Ordering;
-use embassy_usb::{
-    Handler,
-    class::hid::{ReportId, RequestHandler},
-    control::OutResponse,
-};
+use embassy_usb::Handler;
 
 use super::SUSPENDED;
 
@@ -27,23 +23,5 @@ impl Handler for UsbDeviceHandler {
         } else {
             SUSPENDED.store(false, Ordering::Release);
         }
-    }
-}
-
-pub struct UsbRequestHandler {}
-
-impl RequestHandler for UsbRequestHandler {
-    fn get_report(&mut self, _id: ReportId, _buf: &mut [u8]) -> Option<usize> {
-        None
-    }
-
-    fn set_report(&mut self, _id: ReportId, _data: &[u8]) -> OutResponse {
-        OutResponse::Accepted
-    }
-
-    fn set_idle_ms(&mut self, _id: Option<ReportId>, _dur: u32) {}
-
-    fn get_idle_ms(&mut self, _id: Option<ReportId>) -> Option<u32> {
-        None
     }
 }
