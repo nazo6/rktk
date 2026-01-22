0f16630ef042232a696547cb7c300d853721be35
nazo6 <git@nazo6.dev>
2026-01-23 00:34:29 +0900

    fix rp

--- crates/rktk-drivers-rp/src/flash.rs
+++ crates/rktk-drivers-rp/src/flash.rs
@@ -6,18 +6,11 @@ pub use rktk_drivers_common::storage::flash_sequential_map::sequential_storage;
 macro_rules! init_storage {
     ($storage:ident, $flash:expr, $dma:expr, $size:expr) => {
         let flash = ::embassy_rp::flash::Flash::<_, _, $size>::new($flash, $dma);
-        let flash = $crate::flash::embassy_sync::mutex::Mutex::new(flash);
-        let cache = $crate::flash::embassy_sync::mutex::Mutex::new(
-            $crate::flash::sequential_storage::cache::NoCache::new(),
-        );
 
         const FLASH_START: u32 = 1024 * 1024;
-        const FLASH_END: u32 = 3 * 1024 * 1024;
+        const FLASH_SIZE: u32 = 2 * 1024 * 1024;
 
-        let $storage = $crate::flash::FlashSequentialMapStorage {
-            flash: &flash,
-            flash_range: FLASH_START..FLASH_END,
-            cache: &cache,
-        };
+        let $storage =
+            $crate::flash::FlashSequentialMapStorage::new(flash, FLASH_START, FLASH_SIZE);
     };
 }
